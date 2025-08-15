d480f9602e47acdc2a56034008fe1228cb1d27b1
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-16 02:22:50 +0530

    conversion of datatyps

---
+++ 03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = null //string
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // output: NaN (Not a Number) // and null is converted to 0(means 0 zero)
+
+// 33 => 33
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+let isloggedIn = "JITU";
+let booleanIsLoggedIn = Boolean(isloggedIn);
+//console.log(booleanIsLoggedIn); // output: true
+// 0 => false
+// 1 => true
+// "0" => true
+// "" => false
+// " " => true
+// null => false
+// undefined => false
+// NaN => false
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
