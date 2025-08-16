7ff7ab9e4641174acff5e417f8d34c15bf3202c8
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-16 18:34:40 +0530

    comparisoin-

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -1,5 +1,5 @@
 let score = null //string
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score);
 //console.log(typeof valueInNumber);
@@ -26,5 +26,36 @@ let booleanIsLoggedIn = Boolean(isloggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+// ******** operations ***********
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);8*/
+
+
+let str1 = "ranoaldo"
+let str2 = " x messi"
+
+let str3 = str1+str2
+console.log(str3)
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2+ 2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true)
+//console.log(+"")
+
+
+
 
