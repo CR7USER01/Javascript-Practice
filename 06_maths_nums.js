c9ef0bd6adc903a6225a52b3d05059fb1e3d9f3d
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-17 21:21:31 +0530

    maths

---
+++ 06_maths_nums.js
@@ -0,0 +1,68 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(144)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+
+//console.log(typeOf balance);
+
+//console.log(balance.toFixed(1))
+
+const otherNumber = 1123.8966
+
+//console.log(otherNumber.toPrecision(3))
+
+
+const hundreds = 1000000000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++ maths 
+//console.log(Math.PI)
+//console.log(Math)
+//console.log(Math.abs(-4))
+
+//console.log(Math.round(4.5))// most use
+
+//console.log(Math.ceil(4.2))
+//console.log(Math.floor(4.9))
+//console.log(Math.min(888,4,5,6,7))
+//console.log(Math.max(9,78,45,77))
+
+//console.log(Math.random())
+//console.log(Math.random()*10 +1)
+//console.log(Math.floor(Math.random()*10)+ 1)
+
+const max = 10
+
+const min = 20
+
+
+console.log(Math.floor(Math.random() * (max - min +1 )) + min) // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
