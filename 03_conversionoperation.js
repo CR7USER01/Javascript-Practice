6efef8131ffdfc99fd25a6ee6f0181169af2e72f
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-17 12:52:53 +0530

    strings

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -51,7 +51,7 @@ console.log(str3)
 //console.log("1" + 2);
 //console.log(1 + "2");
 //console.log("1" + 2+ 2);
-//console.log(1 + 2 + "2");
+ //console.log(1 + 2 + "2");
 
 //console.log(+true)
 //console.log(+"")
