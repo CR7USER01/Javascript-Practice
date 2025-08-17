21f8c453e20bab7bbde7ff6514498df7118564f3
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-18 00:05:23 +0530

    date_time

---
+++ 07_dates_time.js
@@ -0,0 +1,26 @@
+//  dates
+
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString)
+console.log(myDate.toLocaleString())
+console.log(myDate.toDateString())
+console.log(myDate.toJSON())
+console.log(typeof myDate)
+
+let myCreatedDate = new Date(2023, 0, 23, )
+console.log(myCreatedDate) = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate) = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
+
+
+
+
+
+
+
