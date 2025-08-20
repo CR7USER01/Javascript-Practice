821e4e30e503f453299d23a6b66281e87afd7d43
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-20 20:20:46 +0530

    json.api

---
+++ 12_object_destucturingf_json_api.js
@@ -0,0 +1,33 @@
+// both array and object have destructuring
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+
+}
+
+//course.courseInstructure
+
+const  {courseInstructor:instructor} = course
+
+console.log(instructor);
+
+//const {courseInstructor} = course
+
+//const navbar = ({company}) => {
+//}       for react js 
+
+
+{
+    name:"cr7"
+    coursename:"js in hindi"
+    price: "free"
+}
+
+[
+    {},
+    {},
+    {},
+]
+
