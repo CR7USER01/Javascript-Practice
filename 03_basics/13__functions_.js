6679165ba61d1cf1d85b6c5fe124e3f60aedf4ab
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-08-31 13:11:00 +0530

    func

---
+++ 03_basics/13__functions_.js
@@ -0,0 +1,48 @@
+/*console.log("R")
+console.log("O")
+console.log("N")
+console.log("A")
+console.log("L")
+console.log("D")
+console.log("O")*/
+
+function sayMyName(){
+
+    console.log("R")
+//console.log("O")
+//console.log("N")
+//console.log("A")
+//console.log("L")
+//console.log("D")
+//console.log("O")
+
+
+}
+
+//sayMyName()
+
+//function addTwoNumbers (number1 , number2 ){
+  //console.log(number1 + number2)
+//console.log(number1 + number2 )
+//}
+function addTwoNumbers (number1, number2 ){
+
+    let result = number1 +number2
+    //console.log("Hitesh")
+    //return result
+return number1 + number2
+}
+const result = addTwoNumbers(67 , 3)
+//console.log("Result: ", result)
+
+//function loginUserMessage (username){
+   // if(username === undefined){
+       // consoile.log("please enter a username")
+       // return 
+   // }
+    return `${username} just logged in`
+
+//}
+console.log(loginUserMessage("RONALDO X JITU"))
+console.log(loginUserMessage())
+
---
+++ 03_basics/func.js
@@ -0,0 +1,32 @@
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+//console.log(calculateCartPrice(100,200,300,400))
+
+const user = {
+    username:"ronaldo",
+    price:3000
+    
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);    
+}
+
+//handleObject(user)
+handleObject({
+    username:"messi",
+     price:4000
+    })
+    const myNewArray = [1,20000,3,4,5,6,7,8,9]
+
+    function returnSecondValue(getArray){
+        return getArray[1]
+    }
+       // console.log(returnSecondValue(myNewArray))
+
+       console.log(returnSecondValue([100,200,300,400,500]))
+
+    
+
+       
