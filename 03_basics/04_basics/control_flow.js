5e4520f7f78cfe7253017304b64990787c9b6d6c
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-09-06 19:09:14 +0530

    controal_flow

---
+++ 03_basics/04_basics/control_flow.js
@@ -0,0 +1,66 @@
+// if 
+//const isUserLoggedIn = true
+//if (2 == "2") {
+   // console.log("execute code");
+
+// else {
+    //console.log("do not execute code");
+
+//}
+
+//const score = 200
+
+//if (score > 100) {
+    //var power = "fly"// var scope is completely global
+    //let power = "fly"
+    //let power = "fly" // let scope is block level
+    //const power = "fly" // const scope is block level
+   // console.log("you are a pro player");
+   // console.log(`user power: ${power}`);
+//   }
+    //console.log("you are a pro player");
+   // console.log(`user power: ${power}`)
+
+
+
+//const balance = 1000
+
+//if (balance > 500) console.log ("test"), console.log ("test2")// dont use semicolon here and this is not recomented way to write A PROGRAM 
+
+    //console.log ("test2")
+
+//if (balance < 500){
+    //console.log("less than 500");
+//} else if (balance < 750){
+   // consoile.log("less than 750");
+//} else if (balance < 900){
+    //console.log("less than 900");
+//} else {
+    //console.log("less than 1200");
+//}
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("allow to buy");
+}  
+
+if (loggedInFromEmail || loggedInFromGoogle){
+    console.log("user logged in ");
+}
+
+
+
+
+
+
+
+    
+
+//<,>,<=,>=,==,===(CHECK TYPE),!=(NOT EQUAL),!==
