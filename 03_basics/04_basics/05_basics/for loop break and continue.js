2e3976aced5698b00dc2b5e7c0d87c50f47db61e
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-09-07 05:07:04 +0530

    loop

---
+++ 03_basics/04_basics/05_basics/for loop break and continue.js
@@ -0,0 +1,50 @@
+// for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element === 5) {
+       // console.log("5 is the best number");
+    }
+
+
+   // console.log(element);
+    
+}
+//console.log(element);
+
+for (let i = 0; i < 10; i++) {
+    //console.log(`outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        //console.log(`inner loop value ${j} and inner loop value ${i}`);
+         
+       // console.log(i + '*' + j + '=' + (i * j));
+        
+    }
+    
+}
+
+let myArray = ["flash", "superman", "batman", "aquaman", "wonderwoman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) { // = gives undefined
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+
+if (index === 5) {
+    console.log(`detected 5`);
+    break;  // exit from the loop
+
+    console.log(`value of i is ${index}`);
+
+
+    //const element = array[index];
+    
+}
+
+
+}
