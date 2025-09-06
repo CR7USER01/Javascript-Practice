ef78cfcaa7c89519702d06252614b6ef49d75b4a
CR7USER01 <2201109118_cse@pmec.ac.in>
2025-09-06 19:29:05 +0530

    switch_case

---
+++ 03_basics/04_basics/switch.js
@@ -0,0 +1,50 @@
+// switch case
+// used to perform different actions based on different conditions}
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    
+    
+    case 2:
+        console.log("february");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+  
+    case 4:
+        console.log("april");
+        
+        break;
+  
+  
+    default:
+        console.log("default case");
+        break;
+}
+
+
+switch (month) {
+    case "january":
+        console.log("january");
+        
+        break;
+    case "february":
+        console.log("february");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+
+    default:
+        break;
+}
